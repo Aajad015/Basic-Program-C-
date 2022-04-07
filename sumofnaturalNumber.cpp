78b8baf0160937997905aee41a9c3b45f29d1c75
Aajad015 <52133710+Aajad015@users.noreply.github.com>
2022-04-07 18:07:11 +0530

    Add files via upload

---
+++ sumofnaturalNumber.cpp
@@ -0,0 +1,16 @@
+// Find the Sum of natural Nutural Number
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,sum=0;
+for(i=0;i<=10;i++)
+
+{
+   cout<<i<<"\n";
+     sum= sum+i;
+}
+     cout<<sum<<"Sum  ";
+    return 0;
+}
