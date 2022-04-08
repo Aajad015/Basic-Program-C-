5335831b22ddbbe9177fe75f8bba2a2b574152ef
Aajad015 <52133710+Aajad015@users.noreply.github.com>
2022-04-08 11:13:08 +0530

    Add files via upload

---
+++ Primenumber.cpp
@@ -0,0 +1,22 @@
+#include <iostream>  
+using namespace std;
+int main()
+{
+    int i,n,count=0;
+    cout<<"Enter the any Number: ";
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            count++;
+        }
+    }
+    if(count==2)
+    {
+        cout<<"Prime Number";
+    }
+    else
+    cout<<"Not Prime Number";
+    return 0;
+}
