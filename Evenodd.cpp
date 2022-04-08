b29c61f7fe65f4510c9a4edfd40a27d7cdfd6446
Aajad015 <52133710+Aajad015@users.noreply.github.com>
2022-04-08 12:37:47 +0530

    Add files via upload

---
+++ Evenodd.cpp
@@ -0,0 +1,17 @@
+#include <iostream>  
+using namespace std;
+int main()
+{
+    int i,n;
+    cout<<"Enter the any Number: ";
+    cin>>n;
+    
+      if(n%2==0)
+        {
+            cout<<"Even Number";
+        }
+        else
+        cout<<"Odd Number";
+    return 0;
+    
+}    
